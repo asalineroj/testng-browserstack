f929240ff630164cc40b8808a9269588e3203a9a
andressalinero <andres@browserstack.com>
2021-01-13 00:51:40 +0000

    upgrade sign up

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -12,20 +12,42 @@ public class SingleTest extends BrowserStackTestNGTest {
 
     @Test
     public void test() throws Exception {
-        driver.get("https://www.google.com/");
-        WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("BrowserStack");
-        element.submit();
-        Thread.sleep(5000);
-
+        driver.get("https://open.spotify.com/");
         percy.snapshot("Home Page");
-        nameTest("Search Product");
-        try{
-            assertEquals("BrowserStack - Google Search".toLowerCase(), driver.getTitle().toLowerCase());
-        } catch (AssertionError e){
-            markTest("failed","Title Did not match");
-            throw e;
-        }
+        Thread.sleep(2000);
+//        WebElement element = driver.findElement(By.tagName("SIGN UP"));
+//        element.click();
+//        Thread.sleep(5000);
+//        By.cssSelector("div.wrap>div>header>div>nav>ul>li>a[href='https://www.spotify.com/ie/signup/']")
+//        WebElement element = driver.findElement(By.xpath("//a[@ref='https://www.spotify.com/ie/signup/']"));
+//        element.click();
+        driver.get("https://www.spotify.com/ie/signup/");
+        percy.snapshot("Sign Up Page");
+        Thread.sleep(2000);
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("andres@browserstack.com");
+        WebElement confirm = driver.findElement(By.id("confirm"));
+        confirm.sendKeys("andres@browserstack.com");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("password");
+        WebElement displayname = driver.findElement(By.id("displayname"));
+        displayname.sendKeys("displayname");
+
+        WebElement day = driver.findElement(By.id("day"));
+
+        WebElement month = driver.findElement(By.id("month"));
+
+        WebElement year = driver.findElement(By.id("year"));
+
+        WebElement gender = driver.findElement(By.id("gender"));
+
+        nameTest("Sign Up");
+//        try{
+//            assertEquals("BrowserStack - Google Search".toLowerCase(), driver.getTitle().toLowerCase());
+//        } catch (AssertionError e){
+//            markTest("failed","Title Did not match");
+//            throw e;
+//        }
         markTest("passed","");
         System.out.print("Now To Percy...");
     }
